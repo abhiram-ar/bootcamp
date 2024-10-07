1a1669949f59112c2f8bf3954231110231b6c5ee
abhiram-ar <abhiram2001.ars@gmail.com>
2024-10-08 01:08:45 +0530

    modules

---
+++ week-4/Modules-esm/index.mjs
@@ -0,0 +1,4 @@
+import * as math from "./add.mjs"
+
+let sum = math.add(5,5);
+console.log(sum);
---
+++ week-4/Modules-esm/tempCodeRunnerFile.mjs
@@ -0,0 +1,5 @@
+import * as math from "./add.mjs"
+
+let sum = math.add(5,5);
+let diff = math.sub(5,5);
+console.log(sum, diff);
