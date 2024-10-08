4c6e80bca92f90369c680464617d4fc0fbd8d4cd
abhiram-ar <abhiram2001.ars@gmail.com>
2024-10-08 20:20:10 +0530

    cmd

--- week-4/http-server/server.js
+++ week-4/http-server/server.js
@@ -4,7 +4,9 @@ let server = http.createServer((req, res)=>{
     res.writeHead(200);
     res.end("hello from server");
 })
+//this callback is called when the server recives a request and a request event is emited
 
 server.listen(3000, ()=>{
     console.log("the server is running on port 3000");
 })
+//the callbacks is called when the servers is up and running
