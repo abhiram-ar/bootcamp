82ea4c7ce71fa4afbf4fc6a7d3f370f719469a48
abhiram-ar <abhiram2001.ars@gmail.com>
2024-10-11 19:50:53 +0530

    serverside rendering

--- week-4/view-engien/index.js
+++ week-4/view-engien/index.js
@@ -1,11 +1,56 @@
 const express = require("express");
-const app = express()
+const app = express();
 
+//register viewengine
+app.set("view engine", "ejs");
 
-app.get("/", (req,res)=>{
-    res.sendFile("./views/index.html")
-})
+app.use(express.static("./public"))
 
-app.listen(3000, ()=>{
-    console.log("server is listening of port 3000");
-})
+//rotues
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/home", (req, res) => {
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+      imgURL: "blog (1).jpeg",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+      imgURL: "blog (2).jpeg",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+      imgURL: "blog (3).jpeg",
+    },
+  ];
+
+  res.render("index", { title: "home", name: "abhiram" , blogs });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "about" });
+});
+//redirecting
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "create blog" });
+});
+
+//404 page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "error" });
+});
+
+//server
+app.listen(3000, () => {
+  console.log("server is listening of port 3000");
+});
