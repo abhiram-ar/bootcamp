6173dfbee261d9597f4c1dfdd489fb5b21498b6d
abhiram-ar <abhiram2001.ars@gmail.com>
2024-10-26 14:05:04 +0530

    search bar

--- week-6/week-project/middlewares/auth.js
+++ week-6/week-project/middlewares/auth.js
@@ -1,23 +1,27 @@
-const User = require("../models/User")
+const User = require("../models/User");
 
-const isAuthenticated  = (req, res, next) =>{
-    if(req.session.userId){
-        return next()
+const isAuthenticated = (req, res, next) => {
+    if (req.session.userId) {
+        return next();
     }
-    res.redirect("/login")
-}
+    res.redirect("/login");
+};
 
-const isAdmin = (req, res, next)=>{
-    if(req.session.userId){
-        const user = User.findById(req.session.userId);
-        if(user &&  user.isAdmin){
-            return next()
+const isAdmin = async (req, res, next) => {
+    
+    console.log(`in is admin middleware`);
+    if (req.session.userId) {
+        const user = await User.findById(req.session.userId);
+        console.log(`user data` + user);
+        if (user && user.isAdmin) {
+            return next();
         }
     }
-    res.redirect("/login")
-}
+    console.log("redrecting");
+    res.redirect("/login");
+};
 
 module.exports = {
     isAuthenticated,
-    isAdmin
-}
+    isAdmin,
+};
