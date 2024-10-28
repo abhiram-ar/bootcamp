940f1f459693979aba4daea98ebf1d6774cfb610
abhiram-ar <abhiram2001.ars@gmail.com>
2024-10-28 18:14:16 +0530

    3 sone

--- week-6/nodejs-streamsRevisited/monogotemp.js
+++ week-6/nodejs-streamsRevisited/monogotemp.js
@@ -1,5 +1,21 @@
+//Group students by semester and calculate the average CGPA for each semester.
 db.students.aggregate([
     {
         $group: {_id: "$sem", averageCGPA: {$avg: "$CGPA" }}
     }
+])
+
+
+//Find the total number of students in each degree program, grouped by degree.
+db.students.aggregate([
+    {$group: {_id: "$degree", NumberOfStudens : {$sum:1}}}
+])
+db.students.aggregate([
+    { $group: { _id: "$degree", NumberOfStudens: { $count: {} } } },
+]);
+
+
+//Find the highest CGPA in each degree program.
+db.students.aggregate([
+    {$group: {_id: "$degree", highestCGPA: {$max : "$CGPA"}}}
 ])
