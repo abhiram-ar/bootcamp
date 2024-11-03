0cd3a2ca0e2ad8f1be31b1cde104fbe5f47aa976
abhiram-ar <abhiram2001.ars@gmail.com>
2024-11-03 20:22:15 +0530

    timer

---
+++ week-7/my-react-app/src/Testing.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+export default function Testing() {
+    const [counter, setCounter] = useState(0);
+
+    useEffect(()=>{
+       const timer = setInterval(() => {
+        setCounter(count => count + 1)
+       }, 1000);
+       
+       return (()=>{
+        clearTimeout(timer)
+       })
+       
+    }, [])
+    
+    return(
+        <p>{counter} seconds have passed</p>
+    )
+}
